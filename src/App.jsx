2c4967b135af3a2a5d677c256fe650abdd57bf84
lestrangeqq <lestrangeqq@gmail.com>
2024-08-21 11:46:23 +0300

    Finished basic markup

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,30 @@
 function App() {
-  return <div>HI</div>;
+  return (
+    <section>
+      <Header />
+      <div className="grid grid-cols-[1fr_2fr]">
+        <Navigation />
+        <Content />
+      </div>
+      <Footer />
+    </section>
+  );
+}
+
+function Header() {
+  return <header>[SOCIAL]</header>;
+}
+
+function Navigation() {
+  return <nav>[NAVIGATION]</nav>;
+}
+
+function Footer() {
+  return <footer>[FOOTER]</footer>;
+}
+
+function Content() {
+  return <main>[CONTENT]</main>;
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
-import "./input.css";
+import "./output.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
