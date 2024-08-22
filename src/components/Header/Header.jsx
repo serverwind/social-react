0351acc2d374f43942476c7a335320176ce8e7bf
lestrangeqq <lestrangeqq@gmail.com>
2024-08-22 13:25:13 +0300

    Added link to='/' to logo in header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,9 @@
+import { NavLink } from "react-router-dom";
+
 function Header() {
   return (
     <header className="bg-gray-800 p-4">
-      <span className="text-3xl font-bold text-white">~/.SW</span>
+      <NavLink className="text-3xl font-bold text-white" to="/">~/.SW</NavLink>
     </header>
   );
 }
