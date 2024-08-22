cdc79064528246db1d7c3111b012c9a518a3322b
lestrangeqq <lestrangeqq@gmail.com>
2024-08-22 13:39:06 +0300

    Finished basic html structure/styling of messages page

--- src/components/Messages/Messages.jsx
+++ src/components/Messages/Messages.jsx
@@ -1,5 +1,18 @@
 function Messages() {
-  return ( <div>Messages</div> )
+  return ( 
+    <section className="grid grid-cols-[200px_1fr]">
+      <div className="bg-gray-200 p-4">
+        <ul className="flex flex-col gap-2">
+          <li><a href="#">$Emily</a></li>
+          <li><a href="#">$Jimmy</a></li>
+          <li><a href="#">$Sally</a></li>
+          <li><a href="#">$Maria</a></li>
+          <li><a href="#">$Tommy</a></li>
+        </ul>
+      </div>
+      <div className="bg-gray-100 p-4">No messages</div>
+    </section> 
+  )
 }
 
 export { Messages }
