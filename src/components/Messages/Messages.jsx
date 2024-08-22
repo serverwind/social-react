b4666a3f98838bbba56f5a6165323b80b2bc750c
lestrangeqq <lestrangeqq@gmail.com>
2024-08-22 13:43:10 +0300

    Some minor styling to user names in messenger

--- src/components/Messages/Messages.jsx
+++ src/components/Messages/Messages.jsx
@@ -3,11 +3,11 @@ function Messages() {
     <section className="grid grid-cols-[200px_1fr]">
       <div className="bg-gray-200 p-4">
         <ul className="flex flex-col gap-2">
-          <li><a href="#">$Emily</a></li>
-          <li><a href="#">$Jimmy</a></li>
-          <li><a href="#">$Sally</a></li>
-          <li><a href="#">$Maria</a></li>
-          <li><a href="#">$Tommy</a></li>
+          <li><a href="#"><span className="text-gray-700 font-bold hover:underline transition duration-300">$</span> Emily</a></li>
+          <li><a href="#"><span className="text-gray-700 font-bold hover:underline transition duration-300">$</span> Jimmy</a></li>
+          <li><a href="#"><span className="text-gray-700 font-bold hover:underline transition duration-300">$</span> Sally</a></li>
+          <li><a href="#"><span className="text-gray-700 font-bold hover:underline transition duration-300">$</span> Maria</a></li>
+          <li><a href="#"><span className="text-gray-700 font-bold hover:underline transition duration-300">$</span> Tommy</a></li>
         </ul>
       </div>
       <div className="bg-gray-100 p-4">No messages</div>
