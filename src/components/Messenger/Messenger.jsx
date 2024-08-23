b4acd2a0adcb7c78e44ecbca076624c1115b35eb
lestrangeqq <lestrangeqq@gmail.com>
2024-08-23 21:33:12 +0300

    Add input to messenger

--- src/components/Messenger/Messenger.jsx
+++ src/components/Messenger/Messenger.jsx
@@ -2,7 +2,6 @@ import { User } from "./User/User";
 import { Message } from "./Message/Message";
 
 function Messenger({ messages, users }) {
-
   return (
     <section className="grid grid-cols-[200px_1fr]">
       <div className="bg-gray-200 p-4">
@@ -16,6 +15,10 @@ function Messenger({ messages, users }) {
         {messages.map((message) => (
           <Message key={message.id} message={message.message} />
         ))}
+        <div className="flex gap-2 my-4">
+          <input className="border border-gray-300 p-2" type="text" />
+          <button className="border bg-gray-700 py-2 px-4 text-white text-sm">Send</button>
+        </div>
       </div>
     </section>
   );
