cbcfaaa1ee6cfbd78061704cdd8ab3d658046b4d
lestrangeqq <lestrangeqq@gmail.com>
2024-08-30 15:18:50 +0300

    Post misc styling

--- src/components/Profile/Posts/Post/Post.jsx
+++ src/components/Profile/Posts/Post/Post.jsx
@@ -3,17 +3,23 @@ import { likeActionCreator } from "../../../../data/likeReducer";
 function Post({ id, post, likes, author, dispatch }) {
   function like() {
     likes++;
-    let action = likeActionCreator(id, likes, author = "Alex Lestra");
+    let action = likeActionCreator(id, likes, (author = "Alex Lestra"));
     dispatch(action);
   }
 
   return (
     <div className="mb-4 border border-gray-300 rounded">
       <div className="italic p-4">{post}</div>
-      <div className="bg-gray-200 py-2 px-4">
-        <button onClick={like}><span className="mgc_heart_fill"></span></button>
+      <div className="flex gap-4 bg-gray-200 py-2 px-4 text-sm">
+        <div>
+          <button onClick={like}>
+          <span className="mgc_heart_fill"></span>
+          </button>
         <span> {likes}</span>
-        <span>Author: {author}</span>
+        </div>
+        <div>
+          <span className="mgc_user_2_fill"></span> <span>{author}</span>
+        </div>
       </div>
     </div>
   );
