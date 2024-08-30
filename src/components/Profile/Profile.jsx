c3d6b6c5aeebdc087e4ec0cd661d1bcb703c328f
lestrangeqq <lestrangeqq@gmail.com>
2024-08-30 14:26:32 +0300

    Fixed sidebar

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -4,7 +4,7 @@ import { Posts } from "./Posts/Posts";
 
 function Profile({ posts, dispatch }) {
   return (
-    <main className="flex-shrink-0">
+    <main className="flex-shrink-0 sm:h-screen sm:overflow-y-auto">
       <Brand />
       <Bio />
       <Posts posts={posts} dispatch={dispatch} />
