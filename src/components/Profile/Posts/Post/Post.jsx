56a11593fcbbef4f3f3d7d2584920dd49815b7ea
lestrangeqq <lestrangeqq@gmail.com>
2024-08-30 15:10:34 +0300

    Post styling

--- src/components/Profile/Posts/Post/Post.jsx
+++ src/components/Profile/Posts/Post/Post.jsx
@@ -8,11 +8,11 @@ function Post({ id, post, likes, dispatch }) {
   }
 
   return (
-    <div className="mb-4">
-      <div className="italic mb-2">{post}</div>
-      <div>
-        <button onClick={like}>❤</button>
-        <span>{likes}</span>
+    <div className="mb-4 border border-gray-300 rounded">
+      <div className="italic p-4">{post}</div>
+      <div className="bg-gray-200 py-2 px-4">
+        <button onClick={like}><span className="mgc_heart_fill"></span></button>
+        <span> {likes}</span>
       </div>
     </div>
   );
