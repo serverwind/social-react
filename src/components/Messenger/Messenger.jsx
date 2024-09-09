2ffcb13afc29633601b254d9c96610d0b3a9602e
lestrangeqq <lestrangeqq@gmail.com>
2024-09-09 14:58:15 +0300

    add contextAPI

--- src/components/Messenger/Messenger.jsx
+++ src/components/Messenger/Messenger.jsx
@@ -20,7 +20,7 @@ function Messenger({ stateText, sendMessage, messenger, users }) {
     <section className="grid sm:grid-cols-[200px_1fr]">
       <div className="bg-gray-200 p-4">
         <ul className="flex flex-wrap sm:flex-col justify-center gap-2">
-          {users.map((user) => (
+          {users.users.map((user) => (
             <User key={user.id} name={user.name} chat={`/messenger/${user.id}`} active={(name) => (name.isActive ? s.active : "")} />
           ))}
         </ul>
--- src/components/Messenger/MessengerContainer.jsx
+++ src/components/Messenger/MessengerContainer.jsx
@@ -1,18 +1,28 @@
 import { Messenger } from "./Messenger";
 import { messengerInputActionCreator, sendMessageActionCreator } from "../../data/messengerReducer";
+import { StoreContext } from "../../StoreContext";
 
-function MessengerContainer({ messenger, users, dispatch }) {
+function MessengerContainer() {
+
+  return (
+    <StoreContext.Consumer>
+      {
+        (store) => {
+          let state = store.getState();
   function stateText(text) {
     let action = messengerInputActionCreator(text);
-    dispatch(action);
+    store.dispatch(action);
   }
 
   function sendMessage(id, text) {
     let action = sendMessageActionCreator(id, text);
-    dispatch(action);
+    store.dispatch(action);
   }
-
-  return <Messenger stateText={stateText} sendMessage={sendMessage} messenger={messenger} users={users} />;
+        return <Messenger stateText={stateText} sendMessage={sendMessage} messenger={state.messenger} users={state.users} />;
+        }
+      }
+    </StoreContext.Consumer>
+  )
 }
 
 export { MessengerContainer };
