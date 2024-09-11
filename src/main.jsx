f4d5c76275340b1d5d936093520e887e9499da50
lestrangeqq <lestrangeqq@gmail.com>
2024-09-11 21:22:25 +0300

    removed unnecesary subsrcibe()

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,11 @@ import { Provider } from "react-redux";
 import "./output.css";
 import "../node_modules/mingcute_icon/font/Mingcute.css";
 
-/* DATA */
-
 import { store } from "./data/redux-store";
 
 const root = createRoot(document.getElementById("root"));
 
-function render(state) {
+function render() {
   root.render(
     <StrictMode>
       <Provider store={store}>
@@ -21,8 +19,3 @@ function render(state) {
   );
 }
 render(store.getState());
-
-store.subscribe(() => {
-  let state = store.getState();
-  render(state);
-});
