023b48a110940de05ba46c3d78c9352574c69aeb
lestrangeqq <lestrangeqq@gmail.com>
2024-09-12 20:08:45 +0300

    Users reducer and AC

--- src/data/usersReducer.js
+++ src/data/usersReducer.js
@@ -1,23 +1,36 @@
 const initialState = {
   users: [
-    { name: "Emily", id: "1" },
-    { name: "Jimmy", id: "2" },
-    { name: "Sally", id: "3" },
-    { name: "Maria", id: "4" },
-    { name: "Tommy", id: "5" },
-  ],
-  friends: [
-    { name: "Emily", id: "1" },
-    { name: "Jimmy", id: "2" },
-    { name: "Sally", id: "3" },
+    { name: "Emily", id: "1", friend: true },
+    { name: "Jimmy", id: "2", friend: false },
+    { name: "Sally", id: "3", friend: false },
+    { name: "Maria", id: "4", friend: true },
+    { name: "Tommy", id: "5", friend: true },
   ],
 };
 
 const usersReducer = (state = initialState, action) => {
   switch (action.type) {
+    case "ADD-FRIEND":
+      return {
+        ...state,
+        users: state.users.map((user) => (user.id === action.id ? { ...user, friend: true } : user)),
+      };
+    case "REMOVE-FRIEND":
+      return {
+        ...state,
+        users: state.users.map((user) => (user.id === action.id ? { ...user, friend: false } : user)),
+      };
     default:
       return state;
   }
 };
 
-export { usersReducer };
+function addFriendAC(id) {
+  return { type: "ADD-FRIEND", id: id };
+}
+
+function removeFriendAC(id) {
+  return { type: "REMOVE-FRIEND", id: id };
+}
+
+export { usersReducer, addFriendAC, removeFriendAC };
