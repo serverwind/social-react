efb6e6dafd905e8340891a5b33de37973d83f564
lestrangeqq <lestrangeqq@gmail.com>
2024-09-18 16:51:46 +0300

    Refactoring

--- src/components/Friends/FriendsContainer.jsx
+++ src/components/Friends/FriendsContainer.jsx
@@ -1,6 +1,8 @@
-import { FriendsAPIComponent } from "./FriendsAPIComponent";
+import React from "react";
 import { connect } from "react-redux";
+import axios from "axios";
 import { addFriendAC, removeFriendAC, setUsersAC, setCurrentPageAC, setTotalPagesAC } from "../../data/usersReducer";
+import { Friends } from "./Friends";
 
 function mapDispatchToProps(dispatch) {
   return {
@@ -19,6 +21,36 @@ function mapStateToProps(state) {
     pageSize: state.users.pageSize,
     totalPages: state.users.totalPages,
   };
+}
+
+class FriendsAPIComponent extends React.Component {
+  componentDidMount() {
+    axios.get(`https://social-network.samuraijs.com/api/1.0/users/?page=${this.props.currentPage}&count=${this.props.pageSize}`).then((response) => {
+      this.props.setUsers(response.data.items);
+      this.props.setTotalPages(response.data.totalCount);
+    });
+  }
+
+  onPageChanged = (pageNumber) => {
+    this.props.setCurrentPage(pageNumber);
+    axios.get(`https://social-network.samuraijs.com/api/1.0/users/?page=${pageNumber}&count=${this.props.pageSize}`).then((response) => {
+      this.props.setUsers(response.data.items);
+    });
+  };
+
+  render() {
+    let pagesAmount = Math.ceil(this.props.totalPages / this.props.pageSize);
+    let pages = [];
+    for (let i = 1; i < pagesAmount + 1; i++) {
+      pages.push(i);
+    }
+
+    return (
+      <section>
+        <Friends users={this.props.users} addFriend={this.props.addFriend} removeFriend={this.props.removeFriend} totalPages={this.props.totalPages} pageSize={this.props.pageSize} currentPage={this.props.currentPage} onPageChanged={this.onPageChanged} />
+      </section>
+    );
+  }
 }
 
 const FriendsContainer = connect(mapStateToProps, mapDispatchToProps)(FriendsAPIComponent);
