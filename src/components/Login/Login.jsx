a10714b03d0a3c3e1d069da15b55521864360eb3
lestrangeqq <lestrangeqq@gmail.com>
2024-10-15 16:20:16 +0300

    Login profile link

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,5 +1,9 @@
+import { NavLink } from "react-router-dom";
+
 function Login(props) {
-  return <div className="bg-gray-200 text-sm py-2 px-4 hover:bg-gray-300 transition duration-300">{props.isAuth ? <div>{props.login}</div> : <div>Login</div>}</div>;
+  const profile = `/profile/${props.id}`
+
+  return <div className="bg-gray-200 text-sm py-2 px-4 hover:bg-gray-300 transition duration-300">{props.isAuth ? <NavLink to={profile}>{props.login}</NavLink> : <div>Login</div>}</div>;
 }
 
 export { Login };
