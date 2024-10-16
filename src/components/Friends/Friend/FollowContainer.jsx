d09e6396abad83df9fbca6a2524d314ec1a2046d
lestrangeqq <lestrangeqq@gmail.com>
2024-10-16 15:02:08 +0300

    follow/unfollow

--- src/components/Friends/Friend/FollowContainer.jsx
+++ src/components/Friends/Friend/FollowContainer.jsx
@@ -1,4 +1,5 @@
 import { connect } from "react-redux";
+import axios from "axios";
 import { addFriendAC, removeFriendAC } from "../../../data/usersReducer";
 import { Follow } from "./Follow";
 
@@ -15,11 +16,15 @@ const mapStateToProps = (state) => {
 
 function FollowContainerAPI(props) {
   function onAddFriend() {
-    props.addFriend(props.id);
+    axios.post(`https://social-network.samuraijs.com/api/1.0/follow/${props.id}`, {}, { withCredentials: true, headers: { "API-KEY": "b7ef8bad-0791-4860-931e-92664d" } }).then((response) => {
+      response.data.resultCode === 0 ? props.addFriend(props.id) : null;
+    });
   }
 
   function onRemoveFriend() {
-    props.removeFriend(props.id);
+    axios.delete(`https://social-network.samuraijs.com/api/1.0/follow/${props.id}`, { withCredentials: true, headers: { "API-KEY": "b7ef8bad-0791-4860-931e-92664d" } }).then((response) => {
+      response.data.resultCode === 0 ? props.removeFriend(props.id) : null;
+    });
   }
 
   return <Follow {...props} onAddFriend={onAddFriend} onRemoveFriend={onRemoveFriend} />;
