99da4a349f8157d606e169256a194f07c90c5859
lestrangeqq <lestrangeqq@gmail.com>
2024-11-22 15:17:42 +0200

    all forms now use redux form

--- src/components/Messenger/Chat/Chat.tsx
+++ src/components/Messenger/Chat/Chat.tsx
@@ -1,4 +1,4 @@
-import { useRef } from "react";
+import { reduxForm, Field } from "redux-form";
 import Message from "./Message/Message";
 
 type ChatProps = {
@@ -15,36 +15,34 @@ type ChatProps = {
     bg: string;
     text: string;
   };
-  stateText: (text: string) => void;
   sendMessage: (id: number, text: string) => void;
 };
 
-export default function Chat({ messenger, stateText, sendMessage, theme }: ChatProps) {
-  let newMessageText = useRef<HTMLInputElement>(null);
+function ChatForm(props) {
+  return (
+    <form onSubmit={props.handleSubmit} className="flex gap-2 my-4">
+      <Field component="input" className="border border-gray-300 p-2 w-full lg:w-2/5" type="text" name="text" />
+      <button onClick={props.onSendMessage} className="border bg-gray-300">
+        <span className="mgc_send_fill py-2 px-4"></span>
+      </button>
+    </form>
+  );
+}
 
-  function onStateText() {
-    if (newMessageText.current) {
-      let text: string = newMessageText.current.value;
-      stateText(text);
-    }
-  }
+const ReduxChatForm = reduxForm({ form: "messenger" })(ChatForm);
 
-  function onSendMessage() {
+export default function Chat({ messenger, sendMessage, theme }: ChatProps) {
+  function onSendMessage(formData: { text: string }) {
     let id: number = messenger.messages.length + 1;
-    sendMessage(id, messenger.input.text);
+    sendMessage(id, formData.text);
   }
 
   return (
-    <div className={ `${theme.bg} p-4` }>
+    <div className={`${theme.bg} p-4`}>
       {messenger.messages.map((message: { id: number; message: string }) => (
         <Message id={message.id} message={message.message} />
       ))}
-      <div className="flex gap-2 my-4">
-        <input ref={newMessageText} onChange={onStateText} className="border border-gray-300 p-2 w-full lg:w-2/5" type="text" value={messenger.input.text} />
-        <button onClick={onSendMessage} className="border bg-gray-300">
-          <span className="mgc_send_fill py-2 px-4"></span>
-        </button>
-      </div>
+      <ReduxChatForm onSubmit={onSendMessage} />
     </div>
   );
 }
--- src/components/Profile/Posts/Input/Input.tsx
+++ src/components/Profile/Posts/Input/Input.tsx
@@ -1,26 +1,25 @@
-import { useRef } from "react";
+import { reduxForm, Field } from "redux-form";
 
-export default function Input(props) {
-  let newPostText = useRef<HTMLInputElement>(null);
-
-  function onStateText() {
-    if (newPostText.current) {
-      const text: string = newPostText.current.value;
-      props.stateText(text);
-    }
-  }
-
-  function onNewPost() {
-    const id: number = props.posts.posts.length + 1;
-    props.newPost(id, props.posts.input.text, 0, "Alex Lestra", "Today");
-  }
-
+function PostForm(props) {
   return (
-    <div className="flex gap-2 mb-4">
-      <input ref={newPostText} onChange={onStateText} className={ `border border-gray-300 p-2 w-full lg:w-2/5 ${props.theme.bg} ${props.theme.text}` } type="text" value={props.posts.input.text} />
-      <button onClick={onNewPost} className={ `border ${props.theme.bg}` }>
+    <form onSubmit={props.handleSubmit} className="flex gap-2 my-4">
+      <Field className={ `border border-gray-300 p-2 w-full lg:w-2/5 ${props.theme.bg} ${props.theme.text}` } type="text" name="text" component="input" />
+      <button onClick={props.onNewPost} className={ `border ${props.theme.bg}` }>
         <span className="mgc_send_fill py-2 px-4"></span>
       </button>
-    </div>
+    </form>
   );
+}
+
+const ReduxPostForm = reduxForm({ form: "posts" })(PostForm);
+
+export default function Input(props) {
+
+  function onNewPost(formData) {
+    const id: number = props.posts.posts.length + 1;
+    props.newPost(id, formData.text, 0, "Alex Lestra", "Today");
+  }
+
+  return <ReduxPostForm onSubmit={onNewPost} theme={props.theme} />
+  
 }
